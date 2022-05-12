f710d1ba939ccd364381c54163b4dbc2fdd44b79
Ramapriya Doom <stay.funeral@gmail.com>
2022-05-12 13:13:38 +0700

    add dockblocks

--- lib/event/mainlistener.php
+++ lib/event/mainlistener.php
@@ -3,11 +3,17 @@
 namespace Ramapriya\Slim\Event;
 
 use Bitrix\Main\DI\ServiceLocator;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 use Ramapriya\Slim\App;
 use Slim\Factory\AppFactory;
 
 class MainListener
 {
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
     public function OnPageStart()
     {
         $container = ServiceLocator::getInstance();
